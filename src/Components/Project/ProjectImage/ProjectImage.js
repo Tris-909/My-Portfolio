7bbb42660fc6874dfa8a93198eec2adae7d58043
Tri Tran <tranminhtri9090@gmail.com>
2023-08-28 22:03:22 +0700

    Re-build ProjectImage section with ChakraUI

--- src/Components/Project/ProjectImage/ProjectImage.js
+++ src/Components/Project/ProjectImage/ProjectImage.js
@@ -1,63 +1,59 @@
-import React from 'react'
-import styled from 'styled-components';
-import Carousel from 'react-material-ui-carousel'
-import {Paper} from '@material-ui/core'
+import React from "react";
+import Carousel from "react-material-ui-carousel";
+import { Box, Image } from "@chakra-ui/react";
 
-const ProjectImageContainer = styled.div`
-    position: absolute; 
-    top: ${props => props.top};
-    left: ${props => props.left};
-    width: ${props => props.width};
-    height: ${props => props.height};
-    z-index: 10;
+const ProjectImage = ({ imageArray, width, height, top, left }) => {
+  return (
+    <Box
+      position="absolute"
+      zIndex={10}
+      top={top}
+      width={width}
+      left={left}
+      height={height}
+      sx={{
+        "@media (max-width: 1400px)": {
+          position: "relative",
+          top: "0%",
+          left: "0%",
+          marginTop: "5%",
+          width: "85%",
+        },
+      }}
+    >
+      <Carousel
+        autoPlay={false}
+        navButtonsAlwaysVisible={true}
+        animation="slide"
+        interval={300}
+        style={{ height: "100%" }}
+      >
+        {imageArray.map((oneImage, index) => {
+          return (
+            <Image
+              key={index}
+              src={oneImage}
+              alt="project 1"
+              border="5px solid white"
+              width="100%"
+              height="350px"
+              sx={{
+                "@media (max-width: 1400px)": {
+                  height: "500px",
+                },
+                "@media (max-width: 650px)": {
+                  height: "300px",
+                },
+                "@media (max-width: 450px)": {
+                  height: "190px",
+                },
+              }}
+            />
+          );
+        })}
+      </Carousel>
+    </Box>
+  );
+};
 
-    @media (max-width: 1400px) {
-        position: relative;
-        top: 0%;
-        left: 0%;
-        margin-top: 5%;
-
-        width: 85%;
-    }
-`;
-
-const Image = styled.img`
-    width: 100%; 
-    height: 350px;
-    backface-visibility: hidden;
-    -webkit-backface-visibility: hidden; 
-
-
-    @media (max-width: 1400px) {
-        height: 500px;
-    }
-
-    @media (max-width: 650px) {
-        height: 300px;
-    }
-
-    @media (max-width: 450px) {
-        height: 190px;
-    }
-`;
-
-export default function ProjectImage({ imageArray, width, height, top, left }) {
-    return (
-        <ProjectImageContainer top={top} width={width} left={left} height={height}>
-            <Carousel 
-            autoPlay={false} 
-            navButtonsAlwaysVisible={true} 
-            animation="slide" 
-            interval={300}
-            style={{height: '100%'}}>
-                {imageArray.map((oneImage, index) => {
-                    return(
-                        <Paper key={index} style={{ border: '5px solid white' }}>
-                            <Image src={oneImage} alt="project 1" />
-                        </Paper>
-                    );
-                })}
-            </Carousel>
-        </ProjectImageContainer>
-    );
-}
+export default ProjectImage;
